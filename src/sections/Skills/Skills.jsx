d0413142d8efdd96173272de8f54f0bab9380c7c
Richard Raad <richardtraad@icloud.com>
2024-12-20 15:41:04 -0500

    Skills section
    Added the skills section with some skills.

---
+++ src/sections/Skills/Skills.jsx
@@ -0,0 +1,28 @@
+import styles from './SkillsStyles.module.css';
+import checkMarkIcon from '../../assets/checkmark-dark.svg';
+import SkillList from '../../common/SkillList';
+
+function Skills() {
+  return (
+    <section id="skills" className={styles.container}>
+      <h1 className="sectionTitle">Skills</h1>
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Java" />
+        <SkillList src={checkMarkIcon} skill="Python" />
+        <SkillList src={checkMarkIcon} skill="HTML" />
+        <SkillList src={checkMarkIcon} skill="CSS" />
+        <SkillList src={checkMarkIcon} skill="JavaScript" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="React" />
+      </div>
+      <hr />
+      <div className={styles.skillList}>
+        <SkillList src={checkMarkIcon} skill="Git" />
+      </div>
+    </section>
+  );
+}
+
+export default Skills;
