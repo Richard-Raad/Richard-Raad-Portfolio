409912f4c6bee489a29328565245691f6aa4b4ca
Richard Raad <richardtraad@icloud.com>
2024-12-20 22:11:58 -0500

    Fixed Resume

--- src/sections/Hero/Hero.jsx
+++ src/sections/Hero/Hero.jsx
@@ -6,7 +6,7 @@ import githubLight from '../../assets/github-light.svg';
 import linkedinLight from '../../assets/linkedin-light.svg';
 import githubDark from '../../assets/github-dark.svg';
 import linkedinDark from '../../assets/linkedin-dark.svg';
-import CV from '../../assets/Richard_Raad_Resume_2024.pdf';
+import CV from '../../assets/Richard_Raads_Resume_2024.pdf';
 import { useTheme } from '../../common/ThemeContext';
 
 function Hero() {
